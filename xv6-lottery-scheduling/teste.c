60414a8dec5fe1ca404ab50a3d63673474d2071b
Andrew <andrew.gabrielgomes@gmail.com>
2022-06-28 16:53:24 -0300

    adiciona mais dois tipos de testes  com definição de bilhetes no shell

--- xv6-lottery-scheduling/teste.c
+++ xv6-lottery-scheduling/teste.c
@@ -9,11 +9,16 @@ int teste0(void);
 int teste1(void);
 int teste2(void);
 int teste3(void);
-
-
+int teste4(int n, int *v);
+int teste5(int n, int *v);
 
 int main(int argc, char *argv[]) {
     int a;
+    int v[argc];
+
+    for (int i = 0; i < argc; i++) {
+        v[i] = atoi(argv[i]);
+    }
 
     a = atoi(argv[1]);
 
@@ -29,6 +34,12 @@ int main(int argc, char *argv[]) {
     } else if (a == 3) {
         printf(1, "teste SEM definicao de bilhetes e processamento INFINITO\n");
         teste3();
+    } else if (a == 4) {
+        printf(1, "teste COM definicao de bilhetes NO SHELL e processamento FINITO\n");
+        teste4(argc,v);
+    } else if (a == 5) {
+        printf(1, "teste COM definicao de bilhetes NO SHELL e processamento INFINITO\n");
+        teste5(argc,v);
     } else {
         printf(1, "no valid option\n");
     }
@@ -139,3 +150,50 @@ int teste3() {
     }
     exit();
 }
+
+/* teste COM definição de bilhetes NO SHELL e processamento FINITO
+testa usando CTRL + P */
+int teste4(int n, int *v) {
+
+    int pid;
+
+    for (int i = 2; i < n; i++) {
+        pid = fork(v[i]);
+        if (pid == 0) {
+            for (int c = 0; c < 1742000; c++){
+                for (int d = 0; d < 1739000; d++){
+                    c = d * 1234.56 + c % 12346;
+                }
+            }
+            printf(1, "P:%d B:%d\n",getpid(), v[i]);
+            exit();
+        }
+    }
+
+    for (int i = 0; i < n-2; i++) { 
+        wait();
+    }
+    exit();
+}
+
+
+/* teste COM definição de bilhetes NO SHELL e processamento INFINITO
+testa usando CTRL + P */
+int teste5(int n, int *v) {
+
+    int pid;
+
+    for (int i = 2; i < n; i++) {
+        pid = fork(v[i]);
+        if (pid == 0) {
+            for(;;);
+            exit();
+        }
+    }
+
+    for (int i = 0; i < n-2; i++) { 
+        wait();
+    }
+    exit();
+}
+
